9178efeea8f05a0cc80d4b883a577ba3dd343285
Laaazy <38176577+Laaazy@users.noreply.github.com>
2018-12-16 15:57:56 +0800

    Add files via upload

---
+++ newPlayer.pas
@@ -0,0 +1,77 @@
+unit newPlayer;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Grids, ValEdit, StdCtrls,dbconnection;
+
+type
+  TnewPlayerForm = class(TForm)
+    playerNameEdit: TEdit;
+    playerSalaryEdit: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    addPlayerButton: TButton;
+    procedure addPlayerButtonClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  newPlayerForm: TnewPlayerForm;
+
+implementation
+
+{$R *.dfm}
+
+{新球员加入联盟}
+procedure TnewPlayerForm.addPlayerButtonClick(Sender: TObject);
+var
+  newPlayerName:String;
+  newPlayerSalary:String;
+  sqlStr:String;
+  success:integer;
+begin
+   success:=0;
+   newPlayerName:=playerNameEdit.Text;{新球员名}
+   newPlayerSalary:=playerSalaryEdit.Text; {新球员薪水}
+   if (newPlayerName='') or (newPlayerSalary ='') then
+      ShowMessage('球员进入联盟失败，请检查各指标是否正确')
+   else
+   begin
+    with DataModule2.ADOQuery1 do
+    begin
+    Close;
+    SQL.Clear;
+    sqlStr:='select * from market where name=:newName';
+    SQL.Add(sqlStr);
+    Prepared:=true;
+    Parameters.ParamByName('newName').Value:=newPlayerName;
+    Open;
+    First;
+    if FieldByName('name').AsString=newPlayerName then
+       ShowMessage('球员已经存在！')
+    else
+    begin
+    Close;
+    SQL.Clear;
+    sqlStr:='insert into market values(''球员'',:name,:salary)';
+    SQL.Add(sqlStr);
+    Prepared:=true;
+    Parameters.ParamByName('name').Value:=newPlayerName;
+    Parameters.ParamByName('salary').Value:=newPlayerSalary;
+    //active:=true;
+    success:=ExecSQL;
+    if success>0 then
+      showMessage('球员加入联盟成功')
+    else
+      ShowMessage('球员进入联盟失败，请检查各指标是否正确');
+    end;
+    end;
+   end;
+end;
+
+end.
