147342c1da1fe222d07d4924fd84fe1fac53e0d5
Laaazy <38176577+Laaazy@users.noreply.github.com>
2018-12-17 22:14:29 +0800

    Add files via upload

--- ClientForm.dpr
+++ ClientForm.dpr
@@ -4,29 +4,34 @@ uses
   Forms,
   client in 'client.pas' {Form1},
   dbConnection in 'dbConnection.pas' {DataModule2: TDataModule},
-  main in 'main.pas' {mainForm},
+  main in 'main.pas' {mainFormr},
   playerPart in 'playerPart.pas' {playerPartForm},
   newPlayer in 'newPlayer.pas' {newPlayerForm},
   seeplayer in 'seeplayer.pas' {playerForm},
   coachPart in 'coachPart.pas' {coachPartForm},
   newCoach in 'newCoach.pas' {newCoachForm},
   seeCoach in 'seeCoach.pas' {coachForm},
-  teamPart in 'teamPart.pas' {teamPartForm};
+  teamPart in 'teamPart.pas' {teamPartForm},
+  arenaPart in 'arenaPart.pas' {arenaPartForm},
+  teamInfo in 'teamInfo.pas' {teamInfoForm};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TmainFormr, mainFormr);
   Application.CreateForm(TForm1, Form1);
-  Application.CreateForm(TcoachPartForm, coachPartForm);
-  Application.CreateForm(TmainForm, mainForm);
-  Application.CreateForm(TDataModule2, DataModule2);
+
   Application.CreateForm(TplayerPartForm, playerPartForm);
+  Application.CreateForm(TcoachPartForm, coachPartForm);
+  Application.CreateForm(TDataModule2, DataModule2);
   Application.CreateForm(TnewPlayerForm, newPlayerForm);
   Application.CreateForm(TplayerForm, playerForm);
   Application.CreateForm(TnewCoachForm, newCoachForm);
   Application.CreateForm(TcoachForm, coachForm);
   Application.CreateForm(TteamPartForm, teamPartForm);
+  Application.CreateForm(TarenaPartForm, arenaPartForm);
+  Application.CreateForm(TteamInfoForm, teamInfoForm);
   Application.Run;
 end.
