26570add5f70ce922bc14cec8626cd0f927a319a
Laaazy <38176577+Laaazy@users.noreply.github.com>
2018-12-20 16:12:55 +0800

    Add files via upload

--- ClientForm.dpr
+++ ClientForm.dpr
@@ -4,7 +4,7 @@ uses
   Forms,
   client in 'client.pas' {Form1},
   dbConnection in 'dbConnection.pas' {DataModule2: TDataModule},
-  main in 'main.pas' {mainFormr},
+  main in 'main.pas' {mainForm},
   playerPart in 'playerPart.pas' {playerPartForm},
   newPlayer in 'newPlayer.pas' {newPlayerForm},
   seeplayer in 'seeplayer.pas' {playerForm},
@@ -13,19 +13,21 @@ uses
   seeCoach in 'seeCoach.pas' {coachForm},
   teamPart in 'teamPart.pas' {teamPartForm},
   arenaPart in 'arenaPart.pas' {arenaPartForm},
-  teamInfo in 'teamInfo.pas' {teamInfoForm};
+  teamInfo in 'teamInfo.pas' {teamInfoForm},
+  registPart in 'registPart.pas' {registForm};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TmainFormr, mainFormr);
+  Application.Title := 'NBA联盟管理系统';
+  Application.HelpFile := 'D:\Documents\RAD Studio\Projects\images\1.png';
   Application.CreateForm(TForm1, Form1);
-
-  Application.CreateForm(TplayerPartForm, playerPartForm);
   Application.CreateForm(TcoachPartForm, coachPartForm);
+  Application.CreateForm(TmainForm, mainForm);
   Application.CreateForm(TDataModule2, DataModule2);
+  Application.CreateForm(TplayerPartForm, playerPartForm);
   Application.CreateForm(TnewPlayerForm, newPlayerForm);
   Application.CreateForm(TplayerForm, playerForm);
   Application.CreateForm(TnewCoachForm, newCoachForm);
@@ -33,5 +35,6 @@ begin
   Application.CreateForm(TteamPartForm, teamPartForm);
   Application.CreateForm(TarenaPartForm, arenaPartForm);
   Application.CreateForm(TteamInfoForm, teamInfoForm);
+  Application.CreateForm(TregistForm, registForm);
   Application.Run;
 end.
